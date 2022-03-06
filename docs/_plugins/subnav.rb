57c8b3e6b0449daf80ac1ed4568804b60a776bd0
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2022-03-06 16:28:51 -0500

    Add subnav plugin

---
+++ docs/_plugins/subnav.rb
@@ -0,0 +1,18 @@
+require "nokogiri"
+
+class MySubnavGenerator < Jekyll::Generator
+  def generate(site)
+    parser = Jekyll::Converters::Markdown.new(site.config)
+
+    site.pages.each do |page|
+      if page.ext == ".md"
+        doc = Nokogiri::HTML(parser.convert(page['content']))
+        page.data["subnav"] = []
+        doc.css('h2').each do |heading|
+          page.data["subnav"] << { "title" => heading.text, "url" => [page.url, heading['id']].join("#") }
+        end
+      end
+    end
+  end
+end
+
